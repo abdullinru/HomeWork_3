4440b979d508fde39300648ce90547b11a0caeb8
abdullinru <abdullin17021989@gmail.com>
2022-04-28 21:12:07 +0600

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     }
     public static void task1 () {
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS==1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }

4affca2567753cec7df36652004221a0d60c9e84
abdullinru <abdullin17021989@gmail.com>
2022-04-28 21:24:25 +0600

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        task2();
 
     }
-    public static void task1 () {
-        int clientOS = 0;
+    public static void task2 () {
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
         if (clientOS==1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear>=2015){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+            else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
+
         }
         if (clientOS==0) {
-            System.out.println("Установите версию приложения для IOS по ссылке");
+            if (clientDeviceYear>=2015) {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            }
+            else {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }
         }
 
     }

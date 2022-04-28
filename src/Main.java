67aee057f99a2c75ac79be45959471a4969d121b
abdullinru <abdullin17021989@gmail.com>
2022-04-28 22:45:03 +0600

    задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        task4();
+        task5();
 
     }
-    public static void task4 () {
-        int deliveryDistance = 95;
-        int days = 0;
-        if (deliveryDistance < 20){
-            days = 1;
-            System.out.println("Потребуется дней: "+ days);
+    public static void task5 () {
+        int monthNumber  = 12;
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Это зимний месяц");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("это весенний месяц");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это летний месяц");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это осенний месяц");
+                break;
+            default:
+                System.out.println("некорректно введен номер месяца");
+
         }
-        else if (deliveryDistance < 60){
-            days = 2;
-            System.out.println("Потребуется дней: "+ days);
-        }
-        else {
-            days = 3;
-            System.out.println("Потребуется дней: "+ days);
-        }
+
+
     }
 }
